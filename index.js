f6971ab695d58f01e734922cbc51e60911290b73
ferrazf <ferrazf9@gmail.com>
2019-05-02 22:56:37 +0000

    Sum all numbers in an array, including sub-arrays

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  var result = 0;
+
+  for (item of array) {
+    if (Array.isArray(item)) {
+      result += sumItems(item);
+    } else {
+      result += item;
+    }
+  }
+  return result;
 }
 
 module.exports = sumItems;
